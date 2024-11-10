29e14a7dd42c378744fdf806772e921983539559
Praveengaur123 <praveengaur432@gmail.com>
2024-11-10 17:19:23 +0530

    Added cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
@@ -9,18 +10,16 @@ exports.getProducts = (req, res, next) => {
     });
   });
 };
+
 exports.getProduct = (req, res, next) => {
-  const prodID=req.params.productID;
-  Product.findById(prodID,product=>{
-    res.render('shop/product-detail',{
-      product:product,
-      pageTitle:product.title,
-      path:'/products'
-    })
+  const prodId = req.params.productId;
+  Product.findById(prodId, product => {
+    res.render('shop/product-detail', {
+      product: product,
+      pageTitle: product.title,
+      path: '/products'
+    });
   });
-  res.redirect('/')
-  
- 
 };
 
 exports.getIndex = (req, res, next) => {
@@ -38,6 +37,14 @@ exports.getCart = (req, res, next) => {
     path: '/cart',
     pageTitle: 'Your Cart'
   });
+};
+
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId, product => {
+    Cart.addProduct(prodId, product.price);
+  });
+  res.redirect('/cart');
 };
 
 exports.getOrders = (req, res, next) => {
