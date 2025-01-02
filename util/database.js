d1141ea913e3584c95b8b972188fbbbb7080247a
Praveengaur123 <praveengaur432@gmail.com>
2025-01-02 20:52:18 +0530

    user relation

--- util/database.js
+++ util/database.js
@@ -1,7 +1,8 @@
-const  Sequelize  = require("sequelize");
+const Sequelize = require('sequelize');
 
-const sequelize =new Sequelize('nodecomplete','root','Pra0@123',{
-    dialect:'mysql',
-    host:'localhost'
-})
-module.exports=sequelize
+const sequelize = new Sequelize('nodecomplete', 'root', 'Pra0@123', {
+  dialect: 'mysql',
+  host: 'localhost'
+});
+
+module.exports = sequelize;
